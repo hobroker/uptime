ef9eab5109d60ae1b2fd270b604984e1799366af
Igor Leahu <4361921+hobroker@users.noreply.github.com>
2026-02-10 10:48:07 +0200

    showcase components

--- packages/uptime-worker/src/services/statuspage/StatuspageComponentService.ts
+++ packages/uptime-worker/src/services/statuspage/StatuspageComponentService.ts
@@ -24,6 +24,7 @@ export class StatuspageComponentService extends StatuspageBaseService {
           component: {
             name,
             status,
+            showcase: true,
           },
         },
       },
