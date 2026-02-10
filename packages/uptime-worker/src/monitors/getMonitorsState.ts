7b91f9445585170c19f4a77b2eb8d74b0a772781
Igor Leahu <4361921+hobroker@users.noreply.github.com>
2026-02-10 18:49:23 +0200

    add p-limit

--- packages/uptime-worker/src/monitors/getMonitorsState.ts
+++ packages/uptime-worker/src/monitors/getMonitorsState.ts
@@ -1,15 +1,18 @@
+import pLimit from "p-limit";
 import { getSingleMonitorState } from "./getSingleMonitorState";
 import { UptimeState, UptimeWorkerConfig } from "../types";
+
+const limit = pLimit(1);
 
 export const getMonitorsState = async (
   config: UptimeWorkerConfig,
   { env }: { env: Env },
 ) => {
-  const state: UptimeState = [];
-  for (const monitor of config.monitors) {
-    const monitorState = await getSingleMonitorState(monitor, { env });
-    state.push(monitorState);
-  }
+  const state: UptimeState = await Promise.all(
+    config.monitors.map((monitor) =>
+      limit(() => getSingleMonitorState(monitor, { env })),
+    ),
+  );
 
   return state;
 };
